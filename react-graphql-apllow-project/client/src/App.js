8ecafb3117ff0881397b2a3b0c002669d61e1147
DebarunMitra <debarun96mitra@gmail.com>
2020-05-10 17:14:38 +0530

    query using apollo done, cards added, data shown

--- react-graphql-apllow-project/client/src/App.js
+++ react-graphql-apllow-project/client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import logo from './logo.png';
 import './App.css';
 
+import Launches from './Components/Launches';
+
+const client =new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
+
 function App() {
   return (
-    <div className="App">
-     <img src={logo} alt="spaceX-logo" style={{display:"block", width: '300' ,margin: "auto"}} />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="spaceX-logo"
+          style={{ display: "block", width: "300", margin: "auto" }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
