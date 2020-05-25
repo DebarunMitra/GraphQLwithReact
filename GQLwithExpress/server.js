afb6badcd04d45fee47f13528a1cbaa2fdcccffb
DebarunMitra <debarun96mitra@gmail.com>
2020-05-26 00:03:54 +0530

    new update

--- GQLwithExpress/server.js
+++ GQLwithExpress/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const expressGraphQL= require('express-graphql');
+const expressGraphQL = require('express-graphql');
 
 
 const app = express();
 
-app.use('./graphql', expressGraphQL({
-    schema: schema,
-    graphql: true
-}))
+// app.use('/graphql', expressGraphQL({
+//     schema: schema,
+//     graphql: true
+// }));
 
 app.listen(5000, ()=>{
     console.log(`Server Listening 5000 ...`);
