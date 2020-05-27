fc7a23e957cc349366807d403a19b4f19177a85c
DebarunMitra <debarun96mitra@gmail.com>
2020-05-27 14:10:18 +0530

    json server added with data

--- GQLwithExpress/schema.js
+++ GQLwithExpress/schema.js
@@ -9,12 +9,12 @@ const {
 const axios = require("axios");
 
 //hardcode data
-const Customers = [
-  { id: "1", name: "John Doe", email: "john@test.in", age: 18 },
-  { id: "2", name: "Alen Doe", email: "alen@test.in", age: 15 },
-  { id: "3", name: "lucy Doe", email: "lucy@test.in", age: 20 },
-  { id: "4", name: "Arther Doe", email: "arther@test.in", age: 10 }
-];
+// const Customers = [
+//   { id: "1", name: "John Doe", email: "john@test.in", age: 18 },
+//   { id: "2", name: "Alen Doe", email: "alen@test.in", age: 15 },
+//   { id: "3", name: "lucy Doe", email: "lucy@test.in", age: 20 },
+//   { id: "4", name: "Arther Doe", email: "arther@test.in", age: 10 }
+// ];
 
 
 //Customer Type
@@ -40,23 +40,22 @@ const RootQuery= new GraphQLObjectType({
             },
             resolve(parentValue, args){
                 
-                for(let i = 0;i < Customers.length;i++){
-                    if(Customers[i].id == args.id){
-                        return Customers[i];
-                    }
-                }
+                // for(let i = 0;i < Customers.length;i++){
+                //     if(Customers[i].id == args.id){
+                //         return Customers[i];
+                //     }
+                // }
                 
-                // return axios.get('http://localhost:3000/customers/'+ args.id)
-                //     .then(res => res.data);
-
+                return axios.get('http://localhost:4000/Customers/'+ args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                // return axios.get('http://localhost:3000/customers')
-                //     .then(res => res.data);
-                return Customers;
+                return axios.get('http://localhost:4000/Customers')
+                    .then(res => res.data);
+                // return Customers;
             }
         }
     }
