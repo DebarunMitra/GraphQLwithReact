09a938a99a2352dfe4db6ad12801e1851e6c29e6
DebarunMitra <debarun96mitra@gmail.com>
2020-05-27 14:30:56 +0530

    CRUD using mutation added

--- GQLwithExpress/schema.js
+++ GQLwithExpress/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLBoolean,
   GraphQLList,
   GraphQLSchema,
+  GraphQLNonNull
 } = require("graphql");
 const axios = require("axios");
 
@@ -61,6 +62,56 @@ const RootQuery= new GraphQLObjectType({
     }
 });
 
+//Mutation
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .post("http://localhost:4000/Customers", {
+            name: args.name,
+            email: args.email,
+            age: args.age,
+          })
+          .then((res) => res.data);
+      },
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete("http://localhost:4000/Customers/" + args.id)
+          .then((res) => res.data);
+      },
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch("http://localhost:4000/Customers/" + args.id, args)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
